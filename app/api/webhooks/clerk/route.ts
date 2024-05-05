7b11609bfc8ac1f6a3b0edd7de19735f9e1c15e3
Lee <leefarnell@mac.com>
2024-05-05 19:49:03 +0100

    amended route path and file

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,8 +1,7 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { clerkClient, WebhookEvent } from "@clerk/nextjs/server";
-import { use } from "react";
-import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
+import { createUser, updateUser, deleteUser } from "@/lib/actions/user.actions";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
