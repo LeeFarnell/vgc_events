939d858dd69e4727b2fdfba3f761f38e519e35a8
Lee <leefarnell@mac.com>
2024-06-15 23:12:40 +0100

    reinserted logs

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -14,14 +14,13 @@ const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
     // Check to see if this is a redirect back from Checkout
     const query = new URLSearchParams(window.location.search);
     if (query.get("success")) {
-      // console.log("Order placed! You will receive an email confirmation.");
+      console.log("Order placed! You will receive an email confirmation.");
     }
 
     if (query.get("canceled")) {
-      console
-        .log
-        // "Order canceled -- continue to shop around and checkout when you’re ready."
-        ();
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you’re ready."
+      );
     }
   }, []);
 
